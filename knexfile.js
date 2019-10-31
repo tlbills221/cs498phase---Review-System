f318a4f5b0cd60ff84a2d6938c26d7f50525a1a7
tlbills221 <49172826+tlbills221@users.noreply.github.com>
2019-10-30 21:22:44 -0400

    added db connections

--- knexfile.js
+++ knexfile.js
@@ -12,7 +12,9 @@ const common = {
 module.exports = {
   development: Object.assign({
     connection: {
-      database: 'abet_system_dev'
+      database: 'abet_system_dev',
+      user: 'postgres',
+      password: 'cookie'
     },
     seeds: {
       directory: './src/dev/seeds'
@@ -21,7 +23,9 @@ module.exports = {
 
   test: Object.assign({
     connection: {
-      database: 'abet_system_test'
+      database: 'abet_system_test',
+      user: 'postgres',
+      password: 'cookie'
     },
     seeds: {
       directory: './src/test/seeds'
