e97872bbf43b263797c970e14a672693c25b6614
tlbills221 <49172826+tlbills221@users.noreply.github.com>
2019-11-02 14:56:22 -0400

    user.js updated

--- src/main/lib/user.js
+++ src/main/lib/user.js
@@ -1,5 +1,18 @@
-const is_whitelisted = (linkblue_username) => {
+const User = require('../models/User') //relative path to User
+
+const is_whitelisted = async (linkblue_username) => {
   // try to pull user from table using the linkblue
+  const user = await User.query()
+    .findById(linkblue_username)
   // if we got user, return true
+  if (user) {
+    return true
+  }
   // else return false
+  else {
+    return false
+  }
 }
+
+module.exports.is_whitelisted = is_whitelisted
+
