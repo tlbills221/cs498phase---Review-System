9eebbb415ba98dae84ae0b3408985416b5a832d1
tlbills221 <49172826+tlbills221@users.noreply.github.com>
2019-11-02 15:00:28 -0400

    added user.js file with tests for whitelist validation

---
+++ src/test/cases/lib/user.js
@@ -0,0 +1,50 @@
+const user_lib = require('../../../main/lib/user') 
+const { expect } = require('../../chai') 
+const sinon = require('sinon') 
+
+const sandbox = sinon.createSandbox(); 
+
+describe('Lib — User', () => { 
+    describe('is_whitelisted', () => { 
+        afterEach(() => { 
+            sandbox. restore() 
+        })
+        
+        it('returns true when the id is in the table', async () => {
+            // Arrange
+            const User = require('../../../main/models/User')
+            //User.query()
+            sandbox.stub(User, "query").returns({
+                //User.query().findById()
+                findById: sandbox.stub().returns({
+                    //an example user object
+                    id: 1,
+                    linkblue_username: 'egto222'
+                })
+            })
+
+            // Act
+            const result = await user_lib.is_whitelisted('egto222')
+
+            // Assert
+            expect(result).to.true
+        })
+        
+        it('returns false when the id is in the table', async () => {
+            // Arrange
+            const User = require('../../../main/models/User')
+
+            // User.query()
+            sandbox.stub(User, "query").returns({
+                    //User.query().findById()
+                    findById: sandbox.stub().returns(null) //no user obj returned
+            })
+
+            // Act
+            const result = await user_lib.is_whitelisted('egto222')
+
+            // Assert
+            expect(result).to.false
+        })
+    })
+})
