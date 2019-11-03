08b8f286348ab3c62521ce0d65d73963d4be96bf
XxxSexyMan69 <49172865+XxxSexyMan69@users.noreply.github.com>
2019-11-03 15:32:44 -0500

    Update CALARCOde.js

--- src/test/cases/common/CALARCOde.js
+++ src/test/cases/common/CALARCOde.js
@@ -1,1 +1,25 @@
+const cnc = require('../../../main/common/CALARCOde')
+const { expect } = require('../../chai')
 
+describe('common - CALARCOde', () => {
+
+	describe('Purell', () => {
+
+		it('does nothing to good ints', async () => {
+			const result = cnc.Purell("1", 'i')
+
+			expect(slo).to.equal(1)
+		})
+    
+    it('does nothing to good strings', async () => {
+			const result = cnc.Purell("yo", 's')
+
+			expect(slo).to.equal("yo")
+		})
+
+    it('returns NaN on bad int', async () => {
+			const result = cnc.Purell("yo", 'i')
+
+			expect(slo).to.equal(NaN)
+		})
+	})
