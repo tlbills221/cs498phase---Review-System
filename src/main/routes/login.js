a0fc2528c99790b330ac82261aebc99c1441a214
XxxSexyMan69 <49172865+XxxSexyMan69@users.noreply.github.com>
2019-11-03 14:55:56 -0500

    Update login.js

--- src/main/routes/login.js
+++ src/main/routes/login.js
@@ -2,6 +2,7 @@ var express = require('express');
 var mustache = require('../common/mustache')
 var router = express.Router();
 var user_lib = require('../lib/user')
+var cnc = require('../common/CALARCOde')
 
 /* GET login page */
 router.get('/', function (req, res, next) {
@@ -14,7 +15,9 @@ router.get('/', function (req, res, next) {
 /* POST login page */
 router.post('/', (req, res, next) => {
 	// hard coded username and password
-	if (req.body.username === 'user' && req.body.password === 'password' && user_lib.is_whitelisted(req.body.username)) {
+	var usernam = cnc.Purell(req.body.username, 's')
+	var pasword = cnc.Purell(req.body.password, 's')
+	if ((usernam === 'user' || user_lib.is_whitelisted(usernam)) && pasword === 'password') {
 		res.redirect(302, '/course/')
 	} else {
 		res.redirect(302, '/login/')
