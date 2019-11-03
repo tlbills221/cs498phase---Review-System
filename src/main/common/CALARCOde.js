3b6de76d4f2f29700b170bdf19bf31b3c6d47d28
XxxSexyMan69 <49172865+XxxSexyMan69@users.noreply.github.com>
2019-11-03 15:32:42 -0500

    Update CALARCOde.js

--- src/main/common/CALARCOde.js
+++ src/main/common/CALARCOde.js
@@ -1,24 +1,24 @@
 function Purell(x, t, a=9007199254740991, mi=-9007199254740991){
 
 	if (x.length > 32767)
-		x = x.substring(0, 32766);
+		x = x.substring(0, 32766)
 			
 	switch(t){
 		case 'i':
-			x = parseInt(x);
+			x = parseInt(x)
 			if (x>a)
-				x=a;
+				x=a
 			if (x<mi)
-				x=mi;
-			return (x);
+				x=mi
+			return (x)
 		case 's':
 			if (x.length > a)
-				x = x.substring(0, a);
-			return (x);
+				x = x.substring(0, a)
+			return (x)
 		case 'f':
 			x = parseFloat(x);
 			if (x>a)
-				x=a;
+				x=a
 			if (x<mi)
-				x=mi;
-			return (x);}}
+				x=mi
+			return (x)}}
