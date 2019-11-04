48910843fa25b180b2840b2059ebcb1667689c1b
tlbills221 <49172826+tlbills221@users.noreply.github.com>
2019-11-03 19:06:33 -0500

    Add files via upload

--- src/test/cases/common/CALARCOde.js
+++ src/test/cases/common/CALARCOde.js
@@ -8,19 +8,19 @@ describe('common - CALARCOde', () => {
 
 	describe('Purell', () => {
 
-		it('does nothing to good ints', Purell () => {
+		it('does nothing to good ints', async () => {
 			const result = cnc.Purell("1", 'i')
 
 			expect(result).to.equal(1)
 		})
     
-    		it('does nothing to good strings', Purell () => {
+    		it('does nothing to good strings', async () => {
 			const result = cnc.Purell("yo", 's')
 
 			expect(result).to.equal("yo")
 		})
 
-    		it('returns NaN on bad int', Purell () => {
+    		it('returns NaN on bad int', async () => {
 			const result = cnc.Purell("yo", 'i')
 
 			expect(result).to.equal(NaN)
