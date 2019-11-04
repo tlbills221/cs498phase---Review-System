d73835c3f3192d84b0475cb5620cfe5c10c5c075
tlbills221 <49172826+tlbills221@users.noreply.github.com>
2019-11-03 19:04:29 -0500

    update calarCODE.js

---
+++ CALARCOde.js
@@ -0,0 +1,34 @@
+const User = require('../models/User')
+const user_lib = require('../lib/user') 
+
+function Purell(x, t, a=9007199254740991, mi=-9007199254740991){
+
+	if (x.length > 32767)
+		x = x.substring(0, 32766)
+			
+	switch(t){
+		case 'i':
+			x = parseInt(x)
+			if (x>a)
+				x=a
+			if (x<mi)
+				x=mi
+			return (x)
+		case 's':
+			if (x.length > a)
+				x = x.substring(0, a)
+			return (x)
+		case 'f':
+			x = parseFloat(x);
+			if (x>a)
+				x=a
+			if (x<mi)
+				x=mi
+			return (x)}}
+
+const AddUser = async (username) => {
+	if (await user_lib.is_whitelisted(username))
+		return(1)
+	await User.query().insert({ linkblue_username: username })
+	return(0)}
+	
