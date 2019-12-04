855e6d8087293409d69015ec493e84b3e4fa711e
tlbills221 <tlbills221@gmail.com>
2019-12-04 00:10:05 -0500

    LEE upd

--- src/test/cases/lib/webDocs.js
+++ src/test/cases/lib/webDocs.js
@@ -1,53 +1,23 @@
 const { expect } = require('../../chai') 
 const sinon = require('sinon') 
-const chaiHttp = require('chai-http')
+const chai = require('chai')
+	, chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 describe('Remote Docs - hosted separately', () => { 
 	describe('Web Docs', () => { 
-		const host_url = 'https://www.cs.uky.edu/~tlbi226/cs498/about/active_courses.html'
+		const host_url = 'https://www.cs.uky.edu/~tlbi226/cs498/about'
 		it('active_courses.html exists and is accessible', async () => {
-            // Arrange
+            		// Arrange
 			const file = 'active_courses.html'
 			//Act
-			result = chai.request.get('/' + file)
+			chai.request(host_url)
+				.get('/' + file)
+				.then(function(res) {
 			//Assert
-			const expect(result).to.equal.not(null)
-        })
-		
-		it('archived_courses.html exists and is accessible', async () => {
-            // Arrange
-			const file = 'archived_courses.html'
-			//Act
-			result = chai.request.get('/' + file)
-			//Assert
-			const expect(result).to.equal.not(null)
-        })
-		
-		it('assess_score.html exists and is accessible', async () => {
-            // Arrange
-			const file = 'assess_score.html'
-			//Act
-			result = chai.request.get('/' + file)
-			//Assert
-			const expect(result).to.equal.not(null)
-        })
-		
-		it('slo.html exists and is accessible', async () => {
-            // Arrange
-			const file = 'slo.html'
-			//Act
-			result = chai.request.get('/' + file)
-			//Assert
-			const expect(result).to.equal.not(null)
-        })
-		
-		it('course_summary.html exists and is accessible', async () => {
-            // Arrange
-			const file = 'course_summary.html'
-			//Act
-			result = chai.request.get('/' + file)
-			//Assert
-			const expect(result).to.equal.not(null)
-        })
+			expect(res).to.not.have.status(404);
+			})
+        	})
     })
 })
