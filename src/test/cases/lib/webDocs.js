61b0f3aa5fcf0e8f9692af59bbb504423aeea73b
tlbills221 <tlbills221@gmail.com>
2019-12-04 00:11:48 -0500

    Lee upd

--- src/test/cases/lib/webDocs.js
+++ src/test/cases/lib/webDocs.js
@@ -18,6 +18,54 @@ describe('Remote Docs - hosted separately', () => {
 			//Assert
 			expect(res).to.not.have.status(404);
 			})
-        	})
+        })
+		
+		it('archived_courses.html exists and is accessible', async () => {
+            		// Arrange
+			const file = 'archived_courses.html'
+			//Act
+			chai.request(host_url)
+				.get('/' + file)
+				.then(function(res) {
+			//Assert
+			expect(res).to.not.have.status(404);
+			})
+        })
+		
+		it('assess_score.html exists and is accessible', async () => {
+            		// Arrange
+			const file = 'assess_score.html'
+			//Act
+			chai.request(host_url)
+				.get('/' + file)
+				.then(function(res) {
+			//Assert
+			expect(res).to.not.have.status(404);
+			})
+        })
+		
+		it('slo.html exists and is accessible', async () => {
+            		// Arrange
+			const file = 'slo.html'
+			//Act
+			chai.request(host_url)
+				.get('/' + file)
+				.then(function(res) {
+			//Assert
+			expect(res).to.not.have.status(404);
+			})
+        })
+		
+		it('course_summary.html exists and is accessible', async () => {
+            		// Arrange
+			const file = 'course_summary.html'
+			//Act
+			chai.request(host_url)
+				.get('/' + file)
+				.then(function(res) {
+			//Assert
+			expect(res).to.not.have.status(404);
+			})
+        })
     })
 })
