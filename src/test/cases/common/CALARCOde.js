22dc7cdce1dfd5bdb6223b1729dd29f372021b47
XxxSexyMan69 <49172865+XxxSexyMan69@users.noreply.github.com>
2019-12-03 23:12:56 -0500

    Update CALARCOde.js

--- src/test/cases/common/CALARCOde.js
+++ src/test/cases/common/CALARCOde.js
@@ -42,4 +42,34 @@ describe('common - CALARCOde', () => {
             	// Assert
             	expect(result).to.true
         	})
-	})})
+	})
+	
+	describe('randoms', () => {
+		it('returns n random ints if n <= 10' async () => {
+			const result = CALARCOde_common.randoms(5)
+
+			expect(result.length).to.equal(5)
+		})
+		it('returns 10 random ints if 10 < n <= 100' async () => {
+			const result = CALARCOde_common.randoms(11)
+
+			expect(result.length).to.equal(10)
+		})
+		it('returns 10 percent of n random ints if 100 < n' async () => {
+			const result = CALARCOde_common.randoms(110)
+
+			expect(result.length).to.equal(11)
+		})
+	})
+
+	describe('easy', () => {
+		it('always returns the proper HTML' async () => {
+			const result = CALARCOde_common.easy()
+
+			expect(result.length).to.equal("<p><br><center><b><font size='7' color='red'>you are entering student learning outcome evaluations not assignment grades.<p>these reports are important to understand how well a course is preparing students and is not an evaluation of the professor.</font></b></center><br><br>")
+		})
+	})
+
+
+
+})
