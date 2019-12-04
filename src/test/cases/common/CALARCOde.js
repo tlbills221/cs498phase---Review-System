c7153f3cb83e2fea7b0a788b546519b03c8480a4
XxxSexyMan69 <49172865+XxxSexyMan69@users.noreply.github.com>
2019-12-03 23:33:45 -0500

    Update CALARCOde.js

--- src/test/cases/common/CALARCOde.js
+++ src/test/cases/common/CALARCOde.js
@@ -5,44 +5,6 @@ const sinon = require('sinon')
 const sandbox = sinon.createSandbox(); 
 
 describe('common - CALARCOde', () => {
-
-	describe('Purell', () => {
-
-		it('does nothing to good ints', async () => {
-			const result = CALARCOde_common.Purell("1", 'i')
-
-			expect(result).to.equal(1)
-		})
-    
-    		it('does nothing to good strings', async () => {
-			const result = CALARCOde_common.Purell("yo", 's')
-
-			expect(result).to.equal("yo")
-		})
-
-    		it('returns NaN on bad int', async () => {
-			const result = CALARCOde_common.Purell("yo", 'i')
-
-			expect(result).to.equal(NaN)
-		})
-	})
-	
-	describe('AddUser', () => {
-		afterEach(() => { 
-            		sandbox. restore() 
-        	})
-		
-		it('adds users', async () => {
-            	// Arrange
-            	const User = require('../../../main/models/User')
-            	CALARCOde_common.AddUser('egto222')
-            	// Act
-            	const result = await user_lib.is_whitelisted('egto222')
-
-            	// Assert
-            	expect(result).to.true
-        	})
-	})
 	
 	describe('Randoms', () => {
 		it('returns n random ints if n <= 10' async () => {
