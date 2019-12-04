f2c5c60a5c148757b1901823191e68a78712008f
tlbills221 <tlbills221@gmail.com>
2019-12-03 22:15:33 -0500

    Lee upd

--- src/test/cases/lib/webDocs.js
+++ src/test/cases/lib/webDocs.js
@@ -7,13 +7,13 @@ describe('Remote Docs - hosted separately', () => {
 		afterEach(() => { 
             sandbox. restore() 
         })
-		
+		const host_url = 'https://www.cs.uky.edu/~tlbi226/cs498/about/active_courses.html'
 		it('active_courses.html exists and is accessible', async () => {
             // Arrange
-			const url = "https://www.cs.uky.edu/~tlbi226/cs498/about/active_courses.html"
+			const file = 'active_courses.html'
 			//Act
-			const result = 
+			result = chai.request.get('/' + file)
 			//Assert
-			const expect(result).to.equal(expectedNum)
+			const expect(result).to.equal.not(null)
         })
     })
